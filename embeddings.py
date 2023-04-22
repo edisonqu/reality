d99d4c697f74c776d80274411b9068665eb79549
byyou <bowenyou@users.noreply.github.com>
2023-04-22 16:25:08 -0400

    update n_results

--- embeddings.py
+++ embeddings.py
@@ -2,6 +2,7 @@ from chromadb.api import Collection
 from chromadb.utils.embedding_functions import EmbeddingFunction
 from database import database_initialization_and_collection
 
+
 def insert_to_db(collection: Collection, text: str, timestamp: str, embedding_function: EmbeddingFunction):
 
     embeddings = embedding_function([text])
@@ -9,14 +10,16 @@ def insert_to_db(collection: Collection, text: str, timestamp: str, embedding_fu
     collection.add(documents=[text], embeddings=embeddings, ids=ids)
     print(collection.count())
 
-def query_db(collection: Collection, query_text: str, embedding_function: EmbeddingFunction):
+
+def query_db(collection: Collection, query_text: str, embedding_function: EmbeddingFunction, n_results=1):
 
     query_embed = embedding_function([query_text])
 
     results = collection.query(
         query_embeddings=query_embed,
-        n_results=1,
+        n_results=max(min(n_results, collection.count()), 1),
     )
 
     print(results)
 
+    return results
