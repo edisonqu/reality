36edec51a214d9d9716f063010884ae1cb4a3bd8
byyou <bowenyou@users.noreply.github.com>
2023-04-22 15:32:41 -0400

    add query

--- embeddings.py
+++ embeddings.py
@@ -7,3 +7,15 @@ def insert_to_db(collection: Collection, text: str, timestamp: str, embedding_fu
     embeddings = embedding_function([text])
     ids = [timestamp]
     collection.add(documents=[text], embeddings=embeddings, ids=ids)
+
+
+def query_db(collection: Collection, query_text: str, embedding_function: EmbeddingFunction):
+
+    query_embed = embedding_function([query_text])
+
+    results = collection.query(
+        query_embeddings=query_embed,
+        n_results=1,
+    )
+
+    print(results)
