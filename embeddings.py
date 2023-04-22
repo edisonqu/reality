5c16512ca197433976ceb1eee6172ebe9ef15c2b
byyou <bowenyou@users.noreply.github.com>
2023-04-22 13:50:32 -0400

    embedding function

--- embeddings.py
+++ embeddings.py
@@ -1,30 +1,9 @@
-import chromadb
-from chromadb.config import Settings
-from chromadb.utils import embedding_functions
+from chromadb.api import Collection
+from chromadb.utils.embedding_functions import EmbeddingFunction
 
-API_KEY = "MY_API_KEY"
 
-chroma_settings = Settings(
-    chroma_db_impl="duckdb+parquet", persist_directory=".chromadb/")
+def insert_to_db(collection: Collection, text: str, timestamp: str, embedding_function: EmbeddingFunction):
 
-chroma_client = chromadb.Client(chroma_settings)
-
-cohere_ef = embedding_functions.CohereEmbeddingFunction(
-    api_key=API_KEY,  model_name="large")
-
-documents = ["I like burgers", "I hate pizza"]
-
-embeddings = cohere_ef(texts=documents)
-
-metadatas = [{"id": "1", "metadata2": "123"},
-             {"id": "2", "metadata2": "456"}]
-ids = ["id1", "id2"]
-
-collection = chroma_client.get_or_create_collection(
-    name="test_collection", embedding_function=cohere_ef)
-
-collection.add(documents=documents, embeddings=embeddings,
-               metadatas=metadatas, ids=ids)
-
-results = collection.query(query_texts=["burger"], n_results=2)
-print(results)
+    embeddings = embedding_function([text])
+    ids = [timestamp]
+    collection.add(documents=[text], embeddings=embeddings, ids=ids)
